688a069b670b2f157e51b2fc9244227afda829cd
phanducc <phanduc.2915@gmail.com>
2026-04-11 22:17:00 +0700

    fix swcMinify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
+    swcMinify: false,
     images: {
         domains: ['open.cruip.com','ucarecdn.com', 'www.svgrepo.com','images.unsplash.com', 'res.cloudinary.com']
     },
